59b68d80c3e5d85e091d443ccab29c6ba0da44b1
Arianna <ariannameiset@gmail.com>
2023-11-08 15:16:50 -0600

    Base Setup
    Player, player controller, and game environment are set up

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [SerializeField] Transform objectToFollow = null;
+    Vector3 objectOffset;
+
+    private void Awake()
+    {
+        objectOffset = this.transform.position - objectToFollow.position;
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = objectToFollow.position + objectOffset;
+    }
+}
